140edf3f85cdcb84a14a4754e89070a398da6c18
Christopher Comella <git.chrisco@gmail.com>
2016-07-27 15:36:49 -0600

    Has passwords with bcrypt modified:   api/signup.js

--- api/signup.js
+++ api/signup.js
@@ -3,17 +3,23 @@ var router = express.Router();
 var knex = require('../db/knex');
 var db = require('../db/api');
 
+var bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
+
+// Create new user
 router.post('/', function(req, res, next) {
 	db.getUserByPhone(req.body.phone)
 		.then(function(data) {
 			var user = data;
 			if (!user) {
-				db.createUser(req.body)
+        var clearTextPw = req.body.pw;
+        req.body.pw = bcrypt.hashSync(clearTextPw, salt);
+        db.createUser(req.body)
         .then(function(user) {
           res.send(user);
         });
 			} else {
-        res.send(user);
+        res.send({Error: 'User already exists'});
 			}
 		});
 });
